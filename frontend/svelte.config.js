dd2621151b6757d508d0304567484114e3fbad1f
crismon-mint <chrissigrilus@gmail.com>
2023-04-02 06:36:22 +0200

    feat: add new frontend and kubernetes client

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,23 @@
-import sveltePreprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true,
+    }),
+  },
+};
+
+export default config;
